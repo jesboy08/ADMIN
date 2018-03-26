085557f300459c9ed0d0d763de344f7e82bf3489
Jessie Calipara Jamola <jessie.kun8@gmail.com>
2018-03-27 06:43:37 +0800

    Delete createSession.php

--- ajax/createSession.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    if(!isset($_SESSION['user_id'])) {
-        $_SESSION['user_id'] = $_POST['user_id'];
-        $_SESSION['firstname'] = $_POST['firstname'];
-        $_SESSION['lastname'] = $_POST['lastname'];
-        echo true;
-    }
-?>
